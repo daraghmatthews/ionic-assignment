0932883c427829ea48e7948ab81b54c0d0a66895
daraghmatthews <daraghmatthews@gmail.com>
2015-04-27 18:52:51 +0100

    'foodlist'

--- www/js/services.js
+++ www/js/services.js
@@ -13,217 +13,217 @@ angular.module('calorific.services', [])
     id: 1,
     name: 'Bagel',
     text: '289 cal',
-    pic: ''
+    pic: 'img/bagel.jpg'
   }, {
     id: 2,
     name: 'Banana',
     text: 'medium, 105 cal',
-    pic: ''
+    pic: 'img/banana.jpg'
   }, {
     id: 3,
     name: 'Beer',
     text: 'pint (500ml), 153 cal',
-    pic: ''
+    pic: 'img/beer.jpg'
   }, {
     id: 4,
     name: 'Bread',
     text: 'one slice, white, 66 cal',
-    pic: ''
+    pic: 'img/bread.jpg'
   }, {
 	id: 5,
     name: 'Butter',
     text: '1 tablespoon, 102 cal',
-    pic: ''
+    pic: 'img/butter.jpg'
   }, {
     id: 6,
     name: 'Carrots',
     text: 'raw, 1 cup, 52 cal',
-    pic: ''
+    pic: 'img/carrots.jpg'
   }, {
     id: 7,
     name: 'Cheddar cheese',
     text: '1 slice, 113 cal',
-    pic: ''
+    pic: 'img/cheese.jpg'
   }, {
     id: 8,
     name: 'Chicken breast',
     text: 'boneless, skinless, roasted, 3 ounces, 142 cal',
-    pic: ''
+    pic: 'img/chicken.jpg'
   }, {
     id: 9,
     name: 'Chilli with beans',
     text: 'canned, 1 cup, 287 cal',
-    pic: ''
+    pic: 'img/chilli.jpg'
 	}, {
     id: 10,
     name: 'Chocolate chip cookie',
     text: 'from packaged dough, 59 cal',
-    pic: ''
+    pic: 'img/cookie.jpg'
   }, {
     id: 11,
     name: 'Coffee',
     text: 'regular, brewed from grounds, black, 2 cal',
-    pic: ''
+    pic: 'img/coffee.jpg'
   }, {
     id: 12,
     name: 'Cola',
     text: '500ml, 136 cal',
-    pic: ''
+    pic: 'img/cola.jpg'
   }, {
     id: 13,
     name: 'Sweetcorn',
     text: 'canned, whole kernel, drained, 1 cup, 180 cal',
-    pic: ''
+    pic: 'img/corn.jpg'
   }, {
 	id: 14,
     name: 'Egg',
     text: 'large, 102 cal',
-    pic: ''
+    pic: 'img/egg.jpg'
   }, {
     id: 15,
     name: 'Graham cracker',
     text: 'plain, 59 cal',
-    pic: ''
+    pic: 'img/graham.jpg'
   }, {
     id: 16,
     name: 'Granola bar',
     text: '1.5-ounce bar, 193 cal',
-    pic: ''
+    pic: 'img/granola.jpg'
   }, {
     id: 17,
     name: 'Green beans',
     text: 'canned, drained, 1 cup, 40 cal',
-    pic: ''
+    pic: 'img/green-beans.jpg'
   }, {
     id: 18,
     name: 'Ground beef burger',
     text: '4 ounces, fried, 193 cal',
-    pic: ''
+    pic: 'img/burger.jpg'
 	  }, {
     id: 19,
     name: 'Hot dog',
     text: 'pork, 137 cal',
-    pic: ''
+    pic: 'img/hotdog.jpg'
   }, {
     id: 20,
     name: 'Ice cream',
     text: 'vanilla, 110g, 145 cal',
-    pic: ''
+    pic: 'img/ice.jpg'
   }, {
     id: 21,
     name: 'Jam doughnut',
     text: '289 cal',
-    pic: ''
+    pic: 'img/doughnut.jpg'
   }, {
     id: 22,
     name: 'Ketchup',
     text: '1 tablespoon, 15 cal',
-    pic: ''
+    pic: 'img/ketchup.jpg'
   }, {
 	id: 23,
     name: 'Milk',
     text: '2% fat, 250ml, 122 cal',
-    pic: ''
+    pic: 'img/milk.jpg'
   }, {
     id: 24,
     name: 'Peanuts',
     text: 'dry roasted, salted, 30g, 168 cal',
-    pic: ''
+    pic: 'img/peanuts.jpg'
   }, {
     id: 25,
     name: 'Mustard',
     text: 'yellow, 2 teaspoons, 6 cal',
-    pic: ''
+    pic: 'img/mustard.jpg'
   }, {
     id: 26,
     name: 'Porridge',
     text: 'medium bowl, 147 cal',
-    pic: ''
+    pic: 'img/porridge.jpg'
   }, {
     id: 27,
     name: 'Orange juice',
     text: 'made from concentrate, 250ml, 112 cal',
-    pic: ''
+    pic: 'img/orange-juice.jpg'
 	}, {
     id: 28,
     name: 'Peanut butter',
     text: '2 tablespoons, 180 cal',
-    pic: ''
+    pic: 'img/peabutter.jpg'
   }, {
     id: 29,
     name: 'Pizza',
     text: 'pepperoni, one slice, 298 cal',
-    pic: ''
+    pic: 'img/pizza.jpg'
   }, {
     id: 30,
     name: 'Pork chop',
     text: 'grilled, 85g, 221 cal',
-    pic: ''
+    pic: 'img/porkchop.jpg'
   }, {
     id: 31,
     name: 'Potato',
     text: 'medium, baked, 161 cal',
-    pic: ''
+    pic: 'img/potato.jpg'
   }, {
 	id: 32,
     name: 'Potato chips',
     text: '30g, 155 cal',
-    pic: ''
+    pic: 'img/chips.jpg'
   }, {
     id: 33,
     name: 'Raisins',
     text: '40g, 130 cal',
-    pic: ''
+    pic: 'img/raisins.jpg'
   }, {
     id: 34,
     name: 'Red wine',
     text: '1 glass, 123 cal',
-    pic: ''
+    pic: 'img/red.jpg'
   }, {
     id: 35,
     name: 'Rice',
     text: 'white, 1 cup, 205 cal',
-    pic: ''
+    pic: 'img/rice.jpg'
   }, {
     id: 36,
     name: 'salad dressing',
     text: '2 tablespoons, 146 cal',
-    pic: ''
+    pic: 'img/dressing.jpg'
 	  }, {
     id: 37,
     name: 'Salsa',
     text: '110g, 35 cal',
-    pic: ''
+    pic: 'img/salsa.jpg'
   }, {
     id: 38,
     name: 'Shrimp',
     text: '85g, 84 cal',
-    pic: ''
+    pic: 'img/shrimp.jpg'
   }, {
 	id: 39,
     name: 'Spaghetti',
     text: '1 cup, 221 cal',
-    pic: ''
+    pic: 'img/spaghetti.jpg'
   }, {
     id: 40,
     name: 'Spaghetti sauce',
     text: 'marinara, 110g, 92 cal',
-    pic: ''
+    pic: 'img/spaghetti-sauce.jpg'
   }, {
     id: 41,
     name: 'Tuna',
     text: 'canned in water, 85g, 100 cal',
-    pic: ''
+    pic: 'img/tuna.jpg'
   }, {
     id: 42,
     name: 'White wine',
     text: '1 glass, 121 cal',
-    pic: ''
+    pic: 'img/white.jpg'
   }, {
     id: 43,
     name: 'Yellow cake with chocolate frosting',
     text: 'one piece, 243 cal',
-    pic: ''
+    pic: 'img/cake.jpg'
   }];
 
   return {
